fadc2198ca1ba3cbac59f3d9d6f0ac9099b2fcf8
josephyu11600 <josephyu11600@berkeley.edu>
2023-12-28 15:27:20 -0800

    use local storage for date

--- js/bearcountdown.js
+++ js/bearcountdown.js
@@ -1,41 +1,55 @@
-
 var date;
-var dateSet = false;
-function setDate(){
-  date = $('#meetingTime').val();
-  if(date.length > 0){
-    dateSet = true;
-  }
+function setDate() {
+	let date = localStorage.getItem('meetingTime');
+	if (!date) {
+		date = $('#meetingTime').val();
+	}
 }
 function makeTimer() {
-      var endTime;
-      if (dateSet){
-        endTime = new Date(date)
-      } else {
-        endTime = new Date("15 July 2022 19:00:00 GMT-07:00");
-      }
-
-			endTime = (Date.parse(endTime) / 1000);
-
-			var now = new Date();
-			now = (Date.parse(now) / 1000);
-
-			var timeLeft = endTime - now;
-
-			var days = Math.floor(timeLeft / 86400);
-			var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-			var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-			var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-			if (hours < "10") { hours = "0" + hours; }
-			if (minutes < "10") { minutes = "0" + minutes; }
-			if (seconds < "10") { seconds = "0" + seconds; }
-
-			$("#days").html(days + "<span id = &quot;countdowntxt&quot;>Days</span>");
-			$("#hours").html(hours + "<span id = &quot;countdowntxt&quot; >Hours</span>");
-			$("#minutes").html(minutes + "<span id = &quot;countdowntxt&quot;>Minutes</span>");
-			$("#seconds").html(seconds + "<span id = &quot;countdowntxt&quot;>Seconds</span>");
-
+	let endTime;
+	if (date.length) {
+		endTime = new Date(date);
+	} else {
+		// Set endTime to be the current date if no date was chosen.
+		endTime = new Date();
 	}
 
-	setInterval(function() { makeTimer(); }, 100);
+	endTime = Date.parse(endTime) / 1000;
+
+	var now = new Date();
+	now = Date.parse(now) / 1000;
+
+	var timeLeft = endTime - now;
+
+	var days = Math.floor(timeLeft / 86400);
+	var hours = Math.floor((timeLeft - days * 86400) / 3600);
+	var minutes = Math.floor((timeLeft - days * 86400 - hours * 3600) / 60);
+	var seconds = Math.floor(
+		timeLeft - days * 86400 - hours * 3600 - minutes * 60
+	);
+
+	if (hours < '10') {
+		hours = '0' + hours;
+	}
+	if (minutes < '10') {
+		minutes = '0' + minutes;
+	}
+	if (seconds < '10') {
+		seconds = '0' + seconds;
+	}
+
+	$('#days').html(days + '<span id = &quot;countdowntxt&quot;>Days</span>');
+	$('#hours').html(
+		hours + '<span id = &quot;countdowntxt&quot; >Hours</span>'
+	);
+	$('#minutes').html(
+		minutes + '<span id = &quot;countdowntxt&quot;>Minutes</span>'
+	);
+	$('#seconds').html(
+		seconds + '<span id = &quot;countdowntxt&quot;>Seconds</span>'
+	);
+}
+
+setInterval(function () {
+	makeTimer();
+}, 100);
